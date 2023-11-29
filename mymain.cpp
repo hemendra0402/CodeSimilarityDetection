3a856520131dc5253aa9c76a6dcf560e71139355
Derek <Sadler_derek@comcast.net>
2023-11-28 21:58:19 -0800

    Update mymain.cpp

--- mymain.cpp
+++ mymain.cpp
@@ -2,6 +2,7 @@
 #include <clang-c/Index.h>
 #include <vector>
 #include <string>
+#include <iomanip>
 
 // compile
 // my clang file are in my C:\LLVM folder which is why i specify my libraries that way
@@ -10,6 +11,113 @@
 // clang++ -std=c++11 -I/usr/local/opt/llvm/include -L/usr/local/opt/llvm/lib main.cpp -o hello.exe -lclang
 // clang++ -std=c++14 -I "C:\LLVM\include" -L "C:\LLVM\lib" mymain.cpp -o hello.exe -llibclang
 
+
+class Node {
+    public: 
+        Node(const std::string& functionName, const std::string& fileName) : functionName(functionName), fileName(fileName), numCallExpr(0), numParamDecl(0) {}
+        
+        static enum CXChildVisitResult Visitor(CXCursor cursor, CXCursor parent, CXClientData client_data) 
+        {
+            Node* node = static_cast<Node*>(client_data);
+            if (clang_getCursorKind(cursor) == CXCursor_CallExpr) 
+            {
+                node->numCallExpr++;
+            } 
+            else if (clang_getCursorKind(cursor) == CXCursor_ParmDecl) 
+            {
+                node->numParamDecl++;
+            }
+            return CXChildVisit_Recurse;
+        }
+
+        void VisitChildren(CXCursor cursor) 
+        {
+            clang_visitChildren(cursor, Node::Visitor, this);
+        }
+
+        void PrintCounts() const 
+        {
+            std::cout << "File: " << fileName << std::endl;
+            std::cout << "Function: " << functionName << std::endl;
+            std::cout << "Number of CallExpr: " << numCallExpr << std::endl;
+            std::cout << "Number of ParamDecl: " << numParamDecl << std::endl;
+        }
+
+        std::vector<int> GetValues() const
+        {
+            std::vector<int> values = {numCallExpr, numParamDecl};
+            return values;
+        }
+
+        std::string GetFileName() const
+        {
+            return fileName;
+        }
+
+        std::string GetFunctionName() const
+        {
+            return functionName;
+        }
+
+    private:
+        std::string functionName;
+        std::string fileName;
+        int numCallExpr;
+        int numParamDecl;
+};
+
+Node createNode(CXCursor cursor, CXString functionName, CXString fileName)
+{
+    // Pass off the cursor to a different custom "visitor" of the Node class
+    Node node(clang_getCString(functionName), clang_getCString(fileName));
+    node.VisitChildren(cursor);
+
+    return node;
+}
+
+
+// Generic visitor function (for Node finding, not for Node creation)
+CXChildVisitResult generic_visitor(CXCursor cursor, CXCursor parent, CXClientData clientData) {
+    std::vector<Node>& nodeVector = *reinterpret_cast<std::vector<Node>*>(clientData);
+    CXSourceLocation location = clang_getCursorLocation(cursor);
+
+    if (clang_Location_isFromMainFile(location) == 0) {
+        // Continues the cursor traversal with the next sibling of the cursor just 
+        // visited, without visiting its children.
+        return CXChildVisit_Continue;
+    }
+
+    CXCursorKind cursorKind = clang_getCursorKind(cursor);
+    CXString cursorSpelling = clang_getCursorSpelling(cursor);
+    CXString fileName;
+    unsigned line, column;
+    
+    // get location relative to position in file
+    clang_getPresumedLocation(location, &fileName, &line, &column);
+
+    // Check if the cursor kind is a Class
+    if (cursorKind == CXCursor_ClassDecl) {
+        std::cout << "Class: " << clang_getCString(cursorSpelling) << std::endl;
+        // // Perform additional actions here...
+    }
+    // Check if the cursor kind is a Function
+    else if (cursorKind == CXCursor_FunctionDecl) {
+        Node node = createNode(cursor, cursorSpelling, fileName);
+        nodeVector.push_back(node);
+        // Add node to a running list for this source code file, etc.
+        // Do not recursively traverse (handled in Node creation)
+        // Continues the cursor traversal with the next sibling of the cursor just 
+        // visited, without visiting its children.
+        return CXChildVisit_Continue; 
+    }
+
+    // clean up stuff for clang
+    clang_disposeString(cursorSpelling);
+
+    return CXChildVisit_Recurse; 
+}
+
+// not really used more for debugging
 void printAST(CXCursor cursor, int level) {
     // get cursor info
     CXString cursorKind = clang_getCursorKindSpelling(clang_getCursorKind(cursor));
@@ -37,7 +145,7 @@ void printAST(CXCursor cursor, int level) {
         for (int i = 0; i < level; ++i)
             std::cout << "  ";
 
-        std::cout << "Cursor '" << clang_getCString(cursorSpelling) << "' (" << clang_getCString(cursorKind) << ") at " << clang_getCString(fileName) << ":" << line << ":" << column << std::endl;
+        std::cout << "'" << clang_getCString(cursorSpelling) << "' (" << clang_getCString(cursorKind) << ") at " << clang_getCString(fileName) << ":" << line << ":" << column << std::endl;
     }
 
     // dispose of variables
@@ -56,6 +164,7 @@ void printAST(CXCursor cursor, int level) {
     );
 }
 
+// not really used but wanted to keep
 void collectFunctionInfo(CXCursor cursor, std::vector<std::string>& functionInfo) { 
     // if (clang_Location_isFromMainFile(clang_getCursorLocation(cursor)) == 0)
     //     return;  
@@ -100,77 +209,93 @@ void collectFunctionInfo(CXCursor cursor, std::vector<std::string>& functionInfo
     }, &functionInfo);
 }
 
-int main() {
+
+void ReadFile(const char* fileName, std::vector<Node>& nodeVector) 
+{
     CXIndex index = clang_createIndex(0, 0);
-    
-    // Parse first file (file1.cpp)
-    const char *fileName1 = "file1.cpp";
-    const char *args1[] = {"-std=c++11"};
-    CXTranslationUnit translationUnit1 = clang_parseTranslationUnit(
+
+    CXTranslationUnit translation = clang_parseTranslationUnit(
         index,
-        fileName1,
-        args1,
-        sizeof(args1) / sizeof(*args1),
+        fileName,
+        nullptr,
+        0,
         nullptr,
         0,
         CXTranslationUnit_None
     );
-    
-    if (!translationUnit1) {
-        std::cerr << "Unable to parse translation unit for file1. Quitting." << std::endl;
+
+    if (!translation) {
+        std::cerr << "Unable to parse translation unit for " << fileName << " Exiting function" << std::endl;
+        clang_disposeTranslationUnit(translation);
         clang_disposeIndex(index);
-        return 1;
+        return;
     }
 
-    // Parse second file (file2.cpp)
-    const char *fileName2 = "file2.cpp";
-    const char *args2[] = {"-std=c++11"};
-    CXTranslationUnit translationUnit2 = clang_parseTranslationUnit(
-        index,
-        fileName2,
-        args2,
-        sizeof(args2) / sizeof(*args2),
-        nullptr,
-        0,
-        CXTranslationUnit_None
-    );
-    
-    if (!translationUnit2) {
-        std::cerr << "Unable to parse translation unit for file2. Quitting." << std::endl;
-        clang_disposeTranslationUnit(translationUnit1);
-        clang_disposeIndex(index);
-        return 1;
-    }
+    // get the cursor
+    CXCursor cursor = clang_getTranslationUnitCursor(translation);
 
-    // Get cursors for both ASTs
-    CXCursor cursor1 = clang_getTranslationUnitCursor(translationUnit1);
-    CXCursor cursor2 = clang_getTranslationUnitCursor(translationUnit2);
-    printAST(cursor1, 0);
-    printAST(cursor2, 0);
+    // start ast traversal
+    printAST(cursor, 0);
+    std::cout << std::endl;
+    clang_visitChildren(clang_getTranslationUnitCursor(translation), generic_visitor, &nodeVector);
 
-    // Collect function information for file1.cpp
-    std::vector<std::string> functionInfo1;
-    collectFunctionInfo(cursor1, functionInfo1);
-
-    // Collect function information for file2.cpp
-    std::vector<std::string> functionInfo2;
-    collectFunctionInfo(cursor2, functionInfo2);
-
-    // Print function information for file1.cpp
-    std::cout << "Functions in file1.cpp:" << std::endl;
-    for (const auto& info : functionInfo1) {
-        std::cout << info << std::endl;
-    }
-
-    // Print function information for file2.cpp
-    std::cout << "Functions in file2.cpp:" << std::endl;
-    for (const auto& info : functionInfo2) {
-        std::cout << info << std::endl;
-    }
-    // Dispose Translation Units and Index
-    clang_disposeTranslationUnit(translationUnit1);
-    clang_disposeTranslationUnit(translationUnit2);
+    // dispose translation unit and the index for it
+    clang_disposeTranslationUnit(translation);
     clang_disposeIndex(index);
+}
 
+double SimilarityCalculation(const std::vector<int>& function1, const std::vector<int>& function2)
+{
+    double distance = 0.0;
+    double maxdistance = 0.0;
+    
+    // Calculate Euclidean distance
+    for (int i = 0; i < function1.size(); i++) 
+    {
+        double difference = function1[i] - function2[i];
+        distance += pow(difference, 2);
+        maxdistance += pow(std::max(function1[i], function2[i]), 2); // Calculate maximum possible distance
+    }
+    
+    distance = sqrt(distance);
+    maxdistance = sqrt(maxdistance);
+
+    // Calculate similarity score as a percentage
+    double similarity = ((maxdistance - distance) / maxdistance);
+    return similarity;
+}
+
+
+int main() 
+{
+    const char *fileName1 = "file1.cpp";
+    const char *fileName2 = "file2.cpp";
+    std::vector<Node> file1Nodes;
+    std::vector<Node> file2Nodes;
+    ReadFile(fileName1, file1Nodes);    
+    ReadFile(fileName2, file2Nodes);
+
+    // columns
+    std::cout << std::setw(15) << std::right << " "; // Align the first column header
+    for (const auto& file2Node : file2Nodes) 
+    {
+        std::cout << std::setw(15) << std::right << file2Node.GetFunctionName();
+    }
+    std::cout << std::endl;
+
+    // Print out function info with aligned columns
+    for (const auto& file1Node : file1Nodes) 
+    {
+        std::vector<int> file1Vector = file1Node.GetValues();
+        std::cout << std::setw(15) << std::right << file1Node.GetFunctionName();
+
+        for (const auto& file2Node : file2Nodes) 
+        {
+            std::vector<int> file2Vector = file2Node.GetValues();
+            double similarity = SimilarityCalculation(file1Vector, file2Vector);
+            std::cout << std::setw(15) << std::right << similarity;
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
